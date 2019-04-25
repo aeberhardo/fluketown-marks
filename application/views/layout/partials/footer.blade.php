4f9f52deee17db76be8070f018cce3cdeb79e133
Oliver Aeberhard <oliver.aeberhard@gmail.com>
2019-04-25 20:42:49 +0200

    fixed  php4/5 constructor compatibility

--- application/views/layout/partials/footer.blade.php
+++ application/views/layout/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div id="footer">
     <div class="container">
         <center>
-            <p class="muted credit">&copy; 2013 - 2015 by <a href="{{ URL::to_action('about') }}">fluketown.ch</a></p>
+            <p class="muted credit">&copy; 2013 - 2019 by <a href="{{ URL::to_action('about') }}">fluketown.ch</a></p>
         </center>
     </div>
 </div>
